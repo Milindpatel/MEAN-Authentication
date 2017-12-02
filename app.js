f3f802fad9d46316bb095e72ac8dc4572733f8c7
Milindpatel <milindpatel3959@gmail.com>
2017-12-02 15:31:16 -0500

    added file upto the logout functionality

--- app.js
+++ app.js
@@ -4,38 +4,38 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const passport = require('passport');
 const mongoose = require('mongoose');
-const config = require('./config/database')
+const config = require('./config/database');
 
-// Connect to database
+// Connect To Database
 mongoose.connect(config.database);
 
-//On Connection
+// On Connection
 mongoose.connection.on('connected', () => {
-    console.log("connected to database " + config.database);
+  console.log('Connected to database '+config.database);
 });
 
-//database error if not connected
+// On Error
 mongoose.connection.on('error', (err) => {
-    console.log("Database connection error " + err);
+  console.log('Database error: '+err);
 });
 
 const app = express();
 
-const users = require('./routes/users')
+const users = require('./routes/users');
 
-//port Number
-const port = 3000
+// Port Number
+const port = 3000;
 
-//CORS Middleware
+// CORS Middleware
 app.use(cors());
 
-//Set Static Folder
-app.use(express.static(path.join(__dirname,'public')));
+// Set Static Folder
+app.use(express.static(path.join(__dirname, 'public')));
 
-//Body Parser Middleware
+// Body Parser Middleware
 app.use(bodyParser.json());
 
-//Passport Middleware
+// Passport Middleware
 app.use(passport.initialize());
 app.use(passport.session());
 
@@ -43,11 +43,12 @@ require('./config/passport')(passport);
 
 app.use('/users', users);
 
-//Index Route
-app.get('/', (req,res) => {
-    res.send("Invalid Endpoint")
-})
+// Index Route
+app.get('/', (req, res) => {
+  res.send('Invalid Endpoint');
+});
 
+// Start Server
 app.listen(port, () => {
-    console.log("Server started on port " + port)
-})
+  console.log('Server started on port '+port);
+});
